ad06ffdefa29fa283562fe41f015e1b7d660b0d7
borodinka <95708108+borodinka@users.noreply.github.com>
2023-04-03 10:57:35 +0200

    Updated

--- javascript.js
+++ javascript.js
@@ -1,50 +1,47 @@
-const addition = (a, b) => a + b;
-const subtraction = (a, b) => a - b;
-const multiplication = (a, b) => a * b;
-const division = (a, b) => a / b;
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 document.addEventListener('click', function(event) {
 	const isButton = event.target.nodeName === 'BUTTON';
 
-	if (!isButton) 
-		return;
+	if (!isButton) return;
 
 	const equalsSymbol = document.getElementById('equals');
 	let expression = document.getElementById('input-line');
 	const expressionLength = expression.innerText.length;
 
-	if (expressionLength < 10) {
-		if (event.target !== equalsSymbol) {
-			expression.innerText += event.target.textContent;
-		} else {
-			const separators = ['+', '-', '×', '÷'];
-			for (i of separators) {
-				if (expression.innerText.includes(i)) {
-					let elements = expression.innerText.split(i);
-	
-					switch (i) {
-						case '+':
-							expression.innerText = addition(Number(elements[0]), Number(elements[1]));
-							break;
-						case '-':
-							expression.innerText = subtraction(Number(elements[0]), Number(elements[1]));
-							break;
-						case '×':
-							expression.innerText = multiplication(Number(elements[0]), Number(elements[1]));
-							break;
-						case '÷':
-							expression.innerText = (division(Number(elements[0]), Number(elements[1]))).toFixed(3);
-							break;
-						default:
-							alert('Invalid value!');
-					}
-				}
+	if (expressionLength >= 10) {
+		alert('Number is too large!');
+		return;
+	}
+		
+	if (event.target !== equalsSymbol) {
+		expression.innerText += event.target.textContent;
+	} else {
+		const separators = ['+', '-', '×', '÷'];
+		for (i of separators) {
+			if (!expression.innerText.includes(i)) continue;
+				let elements = expression.innerText.split(i);
+
+			switch (i) {
+				case '+':
+					expression.innerText = add(Number(elements[0]), Number(elements[1]));
+					break;
+				case '-':
+					expression.innerText = substract(Number(elements[0]), Number(elements[1]));
+					break;
+				case '×':
+					expression.innerText = multipl(Number(elements[0]), Number(elements[1]));
+					break;
+				case '÷':
+					expression.innerText = (divide(Number(elements[0]), Number(elements[1]))).toFixed(3);
+					break;
+				default:
+					alert('Invalid value!');
 			}
 		}
-	} else {
-		alert('Number is too large!');
 	}
 });
-
-
 
